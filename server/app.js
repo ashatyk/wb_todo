a9fbd73b59ff6b114765dbf0fbe9ec7b65fd0777
ashatyk <kiva.dial@gmail.com>
2023-08-24 09:54:51 +0300

    feat(server) remove logger

--- server/app.js
+++ server/app.js
@@ -33,37 +33,6 @@ app.use((err, req, res) => {
   res.status(err.status || 500);
 });
 
-// Log all endpoints to the console
-function print (path, layer) {
-  if (layer.route) {
-    layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))))
-  } else if (layer.name === 'router' && layer.handle.stack) {
-    layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))))
-  } else if (layer.method) {
-    console.log('%s /%s',
-        layer.method.toUpperCase(),
-        path.concat(split(layer.regexp)).filter(Boolean).join('/'))
-  }
-}
-
-function split (thing) {
-  if (typeof thing === 'string') {
-    return thing.split('/')
-  } else if (thing.fast_slash) {
-    return ''
-  } else {
-    var match = thing.toString()
-        .replace('\\/?', '')
-        .replace('(?=\\/|$)', '$')
-        .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//)
-    return match
-        ? match[1].replace(/\\(.)/g, '$1').split('/')
-        : '<complex:' + thing.toString() + '>'
-  }
-}
-
-app._router.stack.forEach(print.bind(null, []))
-
 app.listen(PORT, () => {
   devServerLog('info', `mock server started on port ${PORT}`);
 });
